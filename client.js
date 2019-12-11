c3aa3dfb1d6868396917cc40f27b5d590bb4ae25
Gabriel Hawk <gabriel.thomas.hawk@gmail.com>
2019-12-11 10:50:08 -0600

    Fixed Last Function

--- client.js
+++ client.js
@@ -32,6 +32,78 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+function checkEmployeeData(employee) {
+  let newName = employee.name;
+  let bonusPercentage = calculateBonusPercentage(employee);
+  
+  let totalCompensation = Number(employee.annualSalary) * bonusPercentage
+  let totalBonus = Number(employee.annualSalary) + totalCompensation;
+  // totalBonus = parseInt(totalBonus)
+
+
+  
+  
+  
+  
+  
+  let newObject = {
+    name: newName,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  }
+
+
+
+  return newObject;
+
+}
+
+function calculateBonusPercentage(employeeToCalculate){
+  let rating = employeeToCalculate.reviewRating;
+  let totalBonusPercentage = 0;
+  let numberLength = employeeToCalculate.employeeNumber.length;
+  let annualSalary =  employeeToCalculate.annualSalary;
+
+  if (rating <= 2){
+    return totalBonusPercentage;
+  }
+  else if (rating === 3){
+    totalBonusPercentage = 4;
+  }
+  else if (rating === 4){
+    totalBonusPercentage = 6;
+  }
+  else if (rating === 5){
+    totalBonusPercentage = 10;
+  }
+  
+  if (numberLength === 4){
+    totalBonusPercentage = totalBonusPercentage + 5;
+  }
+
+  if (annualSalary > 65000){
+    totalBonusPercentage = totalBonusPercentage -1;
+  }
+
+  if (totalBonusPercentage > 13){
+    totalBonusPercentage = 13;
+  }
+
+  if (totalBonusPercentage < 0){
+    totalBonusPercentage = 0;
+  }
+  return totalBonusPercentage*0.01;
+}
+
+let newEmployeeData = [];
+for (let taco of employees) {
+  
+  newEmployeeData.push(checkEmployeeData(taco));
+  
+  
+}
+
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
@@ -40,4 +112,7 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+console.log(newEmployeeData);
+
+
